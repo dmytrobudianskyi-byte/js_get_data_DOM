ad337e0477a778cce45c386b54381d7314c9265d
Dmytro Budianskyi <dmytro.budianskyi@gmail.com>
2026-09-23 15:56:32 +0200

    completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const data = [...document.body.querySelectorAll('span')];
+
+for (let i = 0; i < data.length; i++) {
+    if (data[i].className !== 'population') {
+        data.splice(i, 1);
+        i--;
+    } else {
+        const normilized = data[i].innerText.replaceAll(',', '');
+        const parsed = Number.parseFloat(normilized);
+
+        if (Number.isNaN(parsed)) {
+            data.splice(i, 1)
+        } else {
+            data[i] = parsed;
+        }
+    }
+}
+
+let totalSum = data.reduce((sum, elem) => sum += elem);
+let avg = totalSum / data.length;
+
+document.body.querySelector('.total-population').innerHTML = totalSum.toLocaleString('en-US');
+document.body.querySelector('.average-population').innerHTML = avg.toLocaleString('en-US');
